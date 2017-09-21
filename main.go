1be29c7139f882124bb7548fe997c6423c07af45
Timothy Higinbottom <timhigins@gmail.com>
2017-09-21 17:46:22 -0400

    Add comments

--- main.go
+++ main.go
@@ -8,13 +8,22 @@ import (
 
 func main() {
 	fmt.Println("Starting app!")
+
+	// Initialize some data
 	pb := shopping.Item{Name: "juice", Type: shopping.Item_DAIRY}
 	fmt.Printf("%+v\n", pb)
+
+	// Create marshaler
 	m := jsonpb.Marshaler{}
 	// m.EmitDefaults = true
+
+	// Marshall from proto-struct to JSON string
 	str, err := m.MarshalToString(&pb)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+
 	fmt.Println(str)
+
+	// All done!
 }
