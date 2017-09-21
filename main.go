2737c9ac30c8ed17a6dcbc0b82df84e5d52e3ced
Timothy Higinbottom <timhigins@gmail.com>
2017-09-21 17:52:14 -0400

    Add test2

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/tim15/protodemo/api/shopping"
+	"os"
 )
 
 func main() {
 	fmt.Println("Starting app!")
 
+	fmt.Println("Running marshall example")
 	// Initialize some data
 	pb := shopping.Item{Name: "juice", Type: shopping.Item_DAIRY}
 	fmt.Printf("%+v\n", pb)
@@ -22,8 +24,21 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	fmt.Println(str)
+
+	fmt.Println("Running unmarshall example")
+	file, err := os.Open("test.json")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	parsed := shopping.Item{}
+	err = jsonpb.Unmarshal(file, &parsed)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Printf("%+v\n", parsed)
 
 	// All done!
 }
